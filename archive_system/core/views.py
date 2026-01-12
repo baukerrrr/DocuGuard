247ff8bbaab28901c76f815ae10dd263f5250788
baukerrrr <raimbauka@mail.ru>
2026-01-12 21:42:13 +0500

    справлен поиск: теперь ищет только с начала слова

--- archive_system/core/views.py
+++ archive_system/core/views.py
@@ -3,11 +3,26 @@ from .models import Document
 
 
 def document_list(request):
-    # Если пользователь вошел (Admin или сотрудник) -> Показываем ВСЁ
-    if request.user.is_authenticated:
+    # 1. Сначала получаем список документов по правам доступа (как раньше)
+    if request.user.is_superuser:
         docs = Document.objects.all()
-    # Если гость -> Показываем только ОБЩИЕ документы
+    elif request.user.is_authenticated:
+        docs = Document.objects.filter(security_level__in=['public', 'internal'])
     else:
         docs = Document.objects.filter(security_level='public')
 
-    return render(request, 'core/document_list.html', {'docs': docs})
+    # 2. ПОИСК (Исправленный для русского языка)
+    search_query = request.GET.get('q', '')
+
+    if search_query:
+        # Превращаем запрос в маленькие буквы (Тест -> тест)
+        query_lower = search_query.lower()
+
+        # Проходимся по списку и ищем совпадения вручную через Python
+        # Это решает проблему с регистром (Заглавные/Строчные)
+        docs = [doc for doc in docs if doc.title.lower().startswith(query_lower)]
+
+    return render(request, 'core/document_list.html', {
+        'docs': docs,
+        'search_query': search_query
+    })
